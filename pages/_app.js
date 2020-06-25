1be33e47b035dcd89816ec42bd4a8c0743736e1c
Mario Retana <maretanar@gmail.com>
2020-06-24 20:45:16 -0600

    Added global styles

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/styles.scss'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function ChapterDayWebsite ({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}

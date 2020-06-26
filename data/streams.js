50be95f552465e8989b286ac5f927f5e33ffcc12
Mario Retana <maretanar@gmail.com>
2020-06-26 13:15:26 -0600

    Added streams data

---
+++ data/streams.js
@@ -0,0 +1,138 @@
+export default [
+  {
+    chapter: 'Melbourne Militia / Acting like a Tasmaniac',
+    band: 'Damage Inc',
+    country: 'Australia',
+    countryCode: 'au',
+    startTime: new Date('2020-06-27T04:00:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Don\'t Tread On Rio',
+    band: 'So What!',
+    country: 'Brazil',
+    countryCode: 'br',
+    startTime: new Date('2020-06-27T08:00:00.000-06:00'),
+    link: 'https://www.facebook.com/groups/dtormc'
+  },
+  {
+    chapter: 'Peruvian Metal Militia',
+    band: 'Whiskey / Minefield',
+    country: 'Peru',
+    countryCode: 'pe',
+    startTime: new Date('2020-06-27T09:00:00.000-06:00'),
+    link: 'https://www.facebook.com/events/258022205489832/'
+  },
+  {
+    chapter: 'Guatemallica',
+    band: 'Dehaan',
+    country: 'Guatemala',
+    countryCode: 'gt',
+    startTime: new Date('2020-06-27T10:00:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Metallitarium',
+    band: 'Seek em All',
+    country: 'España',
+    countryCode: 'es',
+    startTime: new Date('2020-06-27T11:00:00.000-06:00'),
+    link: 'http://www.instagram.com/seekemall/'
+  },
+  {
+    chapter: 'St. Germany / Metal Militia Germany',
+    band: 'My\'tallica',
+    country: 'Germany',
+    countryCode: 'de',
+    startTime: new Date('2020-06-27T12:00:00.000-06:00'),
+    link: 'http://www.mytallica.com/livestream/'
+  },
+  {
+    chapter: 'Metallitarium',
+    band: 'Metalamania',
+    country: 'España',
+    countryCode: 'es',
+    startTime: new Date('2020-06-27T13:00:00.000-06:00'),
+    link: 'http://www.instagram.com/metalmaniaband/'
+  },
+  {
+    chapter: 'Hellasmetallica',
+    band: 'Adrenalin',
+    country: 'Greece',
+    countryCode: 'gr',
+    startTime: new Date('2020-06-27T14:00:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Garage Inc. Colombia / Metallicolombia / The Bogota Straw / The Snake Pit / Lords of Hardwired',
+    band: 'Fisura',
+    country: 'Colombia',
+    countryCode: 'co',
+    startTime: new Date('2020-06-27T03:00:00.000-06:00'),
+    link: 'https://www.facebook.com/events/1393070404209733/'
+  },
+  {
+    chapter: 'Costa Rica Fade To Black',
+    band: 'Whiplash',
+    country: 'Costa Rica',
+    countryCode: 'cr',
+    startTime: new Date('2020-06-27T16:40:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Argentina For All',
+    band: 'MotorBreath',
+    country: 'Argentina',
+    countryCode: 'ar',
+    startTime: new Date('2020-06-27T17:00:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Master of Paraguay',
+    band: 'Overgod',
+    country: 'Paraguay',
+    countryCode: 'py',
+    startTime: new Date('2020-06-27T17:40:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Chilean Destroy',
+    band: 'AnesthesiA',
+    country: 'Chile',
+    countryCode: 'cl',
+    startTime: new Date('2020-06-27T18:20:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Mexican Militia / DGO Gives you Heavy!',
+    band: 'Revenge',
+    country: 'México',
+    countryCode: 'mx',
+    startTime: new Date('2020-06-27T19:00:00.000-06:00'),
+    link: 'https://www.facebook.com/events/265022658079840/'
+  },
+  {
+    chapter: 'Chile To Self Destruct',
+    band: 'Self Destruct',
+    country: 'Chile',
+    countryCode: 'cl',
+    startTime: new Date('2020-06-27T20:00:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Mexitallica',
+    band: 'MexitallicaJam',
+    country: 'México',
+    countryCode: 'mx',
+    startTime: new Date('2020-06-27T21:00:00.000-06:00'),
+    link: 'https://www.facebook.com/damageincmetallica/videos/684207485766824/'
+  },
+  {
+    chapter: 'Utah Metallica Chapter',
+    band: 'The Four Horsemen',
+    country: 'USA',
+    countryCode: 'us',
+    startTime: new Date('2020-06-27T22:00:00.000-06:00'),
+    link: 'https://www.facebook.com/events/182280163203405/'
+  }
+]

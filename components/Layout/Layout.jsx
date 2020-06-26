9ac45d61a2e1720c9f1105954726030af6f543b3
Mario Retana <maretanar@gmail.com>
2020-06-26 10:01:53 -0600

    Added Layout component

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Layout.module.scss'
+
+export default function Layout ({ children }) {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        {children}
+      </main>
+    </div>)
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/styles.scss'
+import Layout from '../components/Layout/Layout'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function ChapterDayWebsite ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }

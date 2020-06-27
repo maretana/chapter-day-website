dfefac3f37a23ca17257dd4c0981f982b507dd9a
Mario Retana <maretanar@gmail.com>
2020-06-27 14:00:15 -0600

    Removed Min threshold

--- utils/consts.js
+++ utils/consts.js
@@ -3,4 +3,4 @@ const minutes = 60 * seconds
 const hours = 60 * minutes
 
 export const STREAM_OLDEST_AGE = -2 * hours
-export const MIN_CURRENT_STREAM_AGE = 1 * minutes
+export const MIN_CURRENT_STREAM_AGE = 0

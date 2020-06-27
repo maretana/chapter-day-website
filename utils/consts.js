509584d810d363c3a4dc303e635af62b0c60e6c1
Mario Retana <maretanar@gmail.com>
2020-06-27 17:39:41 -0600

    Increasing old video threshold

--- utils/consts.js
+++ utils/consts.js
@@ -2,5 +2,5 @@ const seconds = 1000
 const minutes = 60 * seconds
 const hours = 60 * minutes
 
-export const STREAM_OLDEST_AGE = -2 * hours
+export const STREAM_OLDEST_AGE = -3 * hours // Fisura threshold
 export const MIN_CURRENT_STREAM_AGE = 0
